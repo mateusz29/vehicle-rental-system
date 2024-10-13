95b36b47e22be2be6d0228f034cb2c2f5222ecb2
mateusz29 <strzeleckimateusz10@gmail.com>
2024-10-13 23:22:00 +0200

    Finished servlet implementation and added exceptions handling

--- src/main/java/org/example/controller/servlet/ApiServlet.java
+++ src/main/java/org/example/controller/servlet/ApiServlet.java
@@ -1,14 +1,122 @@
 package org.example.controller.servlet;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.user.controller.api.UserController;
+import org.example.user.dto.PutUserRequest;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.UUID;
 
 @WebServlet(urlPatterns = {
         ApiServlet.Paths.API + "/*"
 })
 @MultipartConfig(maxFileSize = 200 * 1024)
-public class ApiServlet {
+public class ApiServlet extends HttpServlet {
+    private UserController userController;
+
     public static final class Paths {
         public static final String API = "/api";
+    }
+
+    public static final class Patterns {
+        private static final Pattern UUID = Pattern.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
+        public static final Pattern USERS = Pattern.compile("/users/?");
+        public static final Pattern USER = Pattern.compile("/users/(%s)".formatted(UUID.pattern()));
+    }
+
+    private final Jsonb jsonb = JsonbBuilder.create();
+
+    @Override
+    public void init() throws ServletException{
+        userController = (UserController) getServletContext().getAttribute("userController");
+    }
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = parseRequestPath(request);
+        String servletPath = request.getServletPath();
+        if (Paths.API.equals(servletPath)) {
+            if (path.matches(Patterns.USERS.pattern())) {
+                response.setContentType("application/json");
+                response.getWriter().write(jsonb.toJson(userController.getUsers()));
+                return;
+            } else if (path.matches(Patterns.USER.pattern())) {
+                response.setContentType("application/json");
+                UUID uuid = extractUuid(Patterns.USER, path);
+                response.getWriter().write(jsonb.toJson(userController.getUser(uuid)));
+                return;
+            }
+        }
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+    }
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = parseRequestPath(request);
+        String servletPath = request.getServletPath();
+        if (Paths.API.equals(servletPath)) {
+            if (path.matches(Patterns.USER.pattern())) {
+                UUID uuid = extractUuid(Patterns.USER, path);
+                userController.deleteUser(uuid);
+                return;
+            }
+        }
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+    }
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = parseRequestPath(request);
+        String servletPath = request.getServletPath();
+        if (Paths.API.equals(servletPath)) {
+            if (path.matches(Patterns.USER.pattern())) {
+                UUID uuid = extractUuid(Patterns.USER, path);
+                PutUserRequest putUserRequest = jsonb.fromJson(request.getReader(), PutUserRequest.class);
+                putUserRequest.setUuid(uuid);
+                userController.updateOrCreateUser(putUserRequest);
+                response.addHeader("Location", createUrl(request, Paths.API + "users" + uuid.toString()));
+                return;
+            }
+        }
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+    }
+
+    private static UUID extractUuid(Pattern pattern, String path) {
+        Matcher matcher = pattern.matcher(path);
+        if (matcher.matches()) {
+            return UUID.fromString(matcher.group(1));
+        }
+        throw new IllegalArgumentException("No UUID in path.");
+    }
+
+    private String parseRequestPath(HttpServletRequest request) {
+        String path = request.getPathInfo();
+        path = path != null ? path : "";
+        return path;
+    }
+
+    public static String createUrl(HttpServletRequest request, String... paths) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(request.getScheme())
+                .append("://")
+                .append(request.getServerName())
+                .append(":")
+                .append(request.getServerPort())
+                .append(request.getContextPath());
+        for (String path : paths) {
+            builder.append("/")
+                    .append(path, path.startsWith("/") ? 1 : 0, path.endsWith("/") ? path.length() - 1 : path.length());
+        }
+        return builder.toString();
     }
 }
